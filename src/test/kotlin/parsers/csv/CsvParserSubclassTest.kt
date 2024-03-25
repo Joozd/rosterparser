c236b37cb725f2f5e19c4a351d48016284ccb634
joozd <euroshopper@gmail.com>
2024-03-25 08:07:37 +0100

    Test improvements

---
+++ src/test/kotlin/parsers/csv/CsvParserSubclassTest.kt
@@ -0,0 +1,44 @@
+package parsers.csv
+
+import nl.joozd.rosterparser.ParsingException
+import nl.joozd.rosterparser.RosterParser
+import nl.joozd.rosterparser.parsers.CSVParser
+import nl.joozd.rosterparser.parsers.csv.JoozdlogV5Parser
+import nl.joozd.rosterparser.parsers.factories.CSVParserConstructor
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
+
+/**
+ * Parent class for standardized CSV Parser Testing.
+ * Tests for correct construction through [parserConstructor] and handles data loading from [testResourceName]
+ *
+ * @property testResourceName Name of the resource in test/resources that holds the test data
+ * @property parserConstructor the [CSVParserConstructor] object that constructs the parser to be tested
+ */
+abstract class CsvParserSubclassTest {
+    abstract val testResourceName: String
+    abstract val parserConstructor: CSVParserConstructor
+
+    protected val parser by lazy { getParser() }
+    private fun getParser() = File(this::class.java.classLoader.getResource("joozdlogv5test.csv")!!.toURI())
+        .inputStream()
+        .use{
+            parserConstructor.createIfAble(it.bufferedReader().readLines())
+        }
+
+    @Test
+    fun testConstructsCorrectly() {
+        // Check bad data handling:
+        assertNull(parserConstructor.createIfAble(listOf("Bad Data")), "Parser should return null for bad data")
+
+        // Check if parser can be made from the data and is of the correct type
+        assertNotNull(parser, "Parser should not be null")
+
+        // Additional type check if needed, replace ExpectedParserType with the expected type
+        assertIs<RosterParser>(parser, "Parser is not of the expected type")
+    }
+}
--- src/test/kotlin/parsers/csv/JoozdlogV5ParserTest.kt
+++ src/test/kotlin/parsers/csv/JoozdlogV5ParserTest.kt
@@ -2,48 +2,40 @@ package parsers.csv
 
 import nl.joozd.rosterparser.ParsedFlight
 import nl.joozd.rosterparser.ParsedSimulatorDuty
-import nl.joozd.rosterparser.ParsingException
-import nl.joozd.rosterparser.RosterParser
 import nl.joozd.rosterparser.parsers.csv.JoozdlogV5Parser
-import java.io.File
+import nl.joozd.rosterparser.parsers.factories.CSVParserConstructor
+import org.junit.jupiter.api.Assertions.assertNotNull
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-import kotlin.test.assertNull
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 
-class JoozdlogV5ParserTest {
-    private val joozdLogV5TestFile = File(this::class.java.classLoader.getResource("joozdlogv5test.csv")!!.toURI())
+class JoozdlogV5ParserTest: CsvParserSubclassTest() {
+    override val testResourceName: String = "joozdlogv5test.csv"
+    override val parserConstructor: CSVParserConstructor = JoozdlogV5Parser
 
     @Test
-    fun testJoozdlogV5Parser(){
-        val lines = joozdLogV5TestFile.readLines()
-        val parser = JoozdlogV5Parser.createIfAble(lines)
-        //check if parser can be made from the data and is of the correct type
-        assert(parser != null)
-        assert(parser is JoozdlogV5Parser)
-        assert(parser is RosterParser)
+    fun testJoozdlogV5Parser() {
+        assertNotNull(parser, "Parser is null, cannot be constructed from test data")
+        assertIs<JoozdlogV5Parser>(parser, "Parser is not an instance of JoozdlogV5Parser")
 
         val parsedRoster = parser!!.getRoster()
 
-        assert(parsedRoster.flights.size == 1)
-        assertEquals(correctParsedFlight, parsedRoster.flights.first())
+        assertEquals(1, parsedRoster.flights.size, "Expected 1 flight in the parsed roster")
+        assertEquals(correctParsedFlight, parsedRoster.flights.first(), "Parsed flight does not match the expected")
 
-        assert(parsedRoster.simulatorDuties.size == 1)
-        assertEquals(correctSimulatorDuty, parsedRoster.simulatorDuties.first())
+        assertEquals(1, parsedRoster.simulatorDuties.size, "Expected 1 simulator duty in the parsed roster")
+        assertEquals(correctSimulatorDuty, parsedRoster.simulatorDuties.first(), "Parsed simulator duty does not match the expected")
 
-        assert(parsedRoster.parsedDuties.size == 2)
-        assert(parsedRoster.timezoneOfRoster == ZoneOffset.UTC)
-        assert(!parsedRoster.flightsArePlanned)
-        assertEquals(correctTimeRange, parsedRoster.coveredTime)
-
-        // check bad data handling:
-        assertNull(JoozdlogV5Parser.createIfAble(listOf("Bad Data")))
-        assertFailsWith<ParsingException> { JoozdlogV5Parser(listOf("Bad Data", "even more bad data")).getRoster() }
+        assertEquals(2, parsedRoster.parsedDuties.size, "Expected 2 duties in the parsed roster")
+        assertEquals(ZoneOffset.UTC, parsedRoster.timezoneOfRoster, "Timezone of roster does not match the expected")
+        assertFalse(parsedRoster.flightsArePlanned, "Expected flightsArePlanned to be false")
+        assertEquals(correctTimeRange, parsedRoster.coveredTime, "Covered time range does not match the expected")
     }
 
     companion object{
