9fe292201502799ccd6af38cbbe0ffd7f72b31ae
joozd <euroshopper@gmail.com>
2024-04-03 12:14:41 +0200

    Simulator type is now optional.

--- src/main/kotlin/ParsedSimulatorDuty.kt
+++ src/main/kotlin/ParsedSimulatorDuty.kt
@@ -7,7 +7,7 @@ import kotlin.time.Duration
  * A simulator duty
  * @property date The date of this session, as a LocalDate. Can be in any timezone, depending on the provided roster.
  * @property duration The Duration of this simulator duty.
- * @property simulatorType The type of simulator used (like B789, or FNPT-2)
+ * @property simulatorType The type of simulator used (like B789, or FNPT-2). Optional.
  * @property remarks Remarks about this simulator duty, like the name of the session.
  * @property persons List of people in this simulator duty. Optional, defaults to null.
  * @property instructionGiven If true, this simulator duty was given as instructor. Optional, defaults to null.
@@ -15,7 +15,7 @@ import kotlin.time.Duration
 data class ParsedSimulatorDuty(
     override val date: LocalDate,
     val duration: Duration,
-    val simulatorType: String,
+    val simulatorType: String? = null,
     val remarks: String? = null,
     val persons: List<Person>? = null,
     val instructionGiven: Boolean? = null
