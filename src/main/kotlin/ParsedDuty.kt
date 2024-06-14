29a86fbb9338af8d317acafc7849af209d917954
joozd <euroshopper@gmail.com>
2024-06-14 09:21:06 +0200

    Made ParsedDuty sealed

--- src/main/kotlin/ParsedDuty.kt
+++ src/main/kotlin/ParsedDuty.kt
@@ -6,6 +6,6 @@ import java.time.LocalDate
  * A ParsedDuty is a duty found on a roster. Probably a [ParsedFlight] or a [ParsedSimulatorDuty]
  * @property date The (starting)date of this duty. The timezone can vary and is specified in the accompanying [ParsedRoster].
  */
-interface ParsedDuty {
+sealed interface ParsedDuty {
     val date: LocalDate
 }
